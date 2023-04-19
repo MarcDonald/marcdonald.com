8eb553a3245817f8a9540fa7b6b141edfffe4260
Marc Donald <dev@marcdonald.com>
2023-04-19 23:04:27 +0100

    Change Home icon in header to profile pic

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,14 @@
-import { GithubIcon, HomeIcon } from 'lucide-react';
+import { GithubIcon } from 'lucide-react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { TypographyLarge } from '@/components/ui/Typography';
+import icon from '@/icon.png';
 
 export default function Header() {
 	return (
 		<header className={'sticky top-0 flex bg-accent p-2'}>
 			<Link href={'/'} className={'rounded-md p-2'}>
-				<HomeIcon />
+				<Image src={icon} alt={''} width={32} height={32} />
 			</Link>
 			<div className={'flex flex-1 items-center justify-center'}>
 				<TypographyLarge className={'text-2xl'}>Marc Donald</TypographyLarge>
